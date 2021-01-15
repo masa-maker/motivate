6ba958eea08cb1effd60154c50c2d007d69328b2
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-15 20:16:54 +0900

    ページ遷移制限

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,23 @@
 class PostsController < ApplicationController
+  before_action :move_to_index, except: :index
+  
   def index
+    @posts = Post.includes(:user).order("created_at DESC")
+    
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
 private
@@ -14,4 +26,9 @@ def post_params
   params.require(:post).permit(:text, :image).merge(user_id: current_user.id)
 end
 
+def move_to_index
+  unless user_signed_in?
+    redirect_to root_path
+  end
+end
 end
