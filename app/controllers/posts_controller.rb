e70d26bc8a5171abe6834785858f13ead58df6b5
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-15 20:44:14 +0900

    投稿編集機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
-  before_action :move_to_index, except: :index
+  before_action :set_item, only: :edit
+  before_action :authenticate_user!, except: [:index]
+  before_action :move_to_index, only: [:edit] 
   
   def index
     @posts = Post.includes(:user).order("created_at DESC")
-    
   end
 
   def new
@@ -20,15 +21,22 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
 private
 
 def post_params
   params.require(:post).permit(:text, :image).merge(user_id: current_user.id)
 end
 
+def set_item
+  @post = Post.find(params[:id])
+end
+
 def move_to_index
-  unless user_signed_in?
-    redirect_to root_path
+  if current_user.id != @post.user_id
+    redirect_to root_path 
   end
 end
 end
