23802015e9e15ea28dbc59959d8a39e139607a9b
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-15 18:49:10 +0900

    投稿ボタン　設置

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,14 @@ class PostsController < ApplicationController
 
   def new
   end
+
+  def create
+  end
+
+private
+
+def post_params
+  params.require(:post).permit(:text, :image).merge(user_id: current_user.id)
+end
+
 end
