df1dc39cedfb768dcc8497b75bb0b59891d864dc
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-16 18:08:12 +0900

    マイページ　フォロー、編集ボタン切り替え実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-
+  before_action :authenticate_user!
   def show
     user = User.find(params[:id])
     @posts = user.posts
+    @user = User.find(params[:id])
   end
 
   def edit
