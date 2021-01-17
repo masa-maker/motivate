e3e02da3639004a0b7c003ca163ea03542dd4629
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-17 10:48:10 +0900

    投稿一覧部分テンプレート　設定

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+  before_action :set_item, only: [:edit, :update]
+  before_action :authenticate_user!
+  before_action :move_to_index,  only: [:index, :edit]
   def index
     @profile = Profile.new
   end
@@ -9,11 +12,9 @@ class ProfilesController < ApplicationController
   end
 
   def edit 
-    @profile = Profile.find(params[:id])
   end
 
   def update
-    @profile = Profile.find(params[:id])
     @profile.update(profile_params)
     redirect_to user_path(@profile.user.id)
   end
@@ -22,5 +23,15 @@ class ProfilesController < ApplicationController
 
   def profile_params
     params.require(:profile).permit(:info, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @profile = Profile.find(params[:id])
+  end
+
+  def move_to_index
+    if current_user.id != @profile.user.id
+      redirect_to root_path
+    end
   end
 end
