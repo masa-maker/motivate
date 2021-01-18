640fe5cc1635112b59766952723d05ee1e1652f4
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-18 12:33:23 +0900

    comment newビューページへ遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "posts#index"
-  resources :posts, only: [:new, :show, :create, :edit, :update, :destroy]
+
+  resources :posts, only: [:new, :show, :create, :edit, :update, :destroy] do
+    resources :comments, only: [:new, :create]
+  end
+
   resources :users, only: [:show] do
     resources :profiles, only: [:new, :create, :edit ,:update] 
   end 
