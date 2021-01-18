846b8a56618fbd205dc7890d0828f099efa074b6
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-18 23:33:05 +0900

    commentモデルアソシエーション再設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  has_many :comments
   has_one :profile
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
