c2115a36b6a0b485f08087031380fcf2a00ca9d6
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-19 10:39:00 +0900

    コメント削除機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
-
+  before_action :set_item, only: :destroy
+  before_action :move_to_index, only: [:create, :destroy] 
+  before_action :only_action, only: :destroy
   def create
     if comment = Comment.create(comment_params) 
       redirect_to post_path(comment.post.id)
@@ -8,9 +10,30 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment.destroy
+    redirect_to post_path(@comment.post.id)
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:comment).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
+
+  def set_item
+    @comment = Comment.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
+
+  def only_action
+    if current_user.id != @comment.user.id
+      redirect_to root_path
+    end
   end
 end
