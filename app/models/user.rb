e61c26445710836d1e0a91019f8a99f71f233db0
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-23 11:09:04 +0900

    フォロー機能メソッドの定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,20 @@ class User < ApplicationRecord
   has_many :followers, through: :reverse_of_relationships, source: :user
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+  
 end
