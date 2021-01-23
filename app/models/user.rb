d0cd6f40808101642bcd6862a77b8849aabc2303
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-23 11:01:08 +0900

    relationshipsモデル　アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,11 @@ class User < ApplicationRecord
   has_many :goods, dependent: :destroy
   has_many :good_posts, through: :goods, source: :post
   has_one :profile
+
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follow_id"
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
