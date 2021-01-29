f4e2a1d223850c272808b2d342d9168541b557e3
masa-maker <76035698+masa-maker@users.noreply.github.com>
2021-01-29 16:04:07 +0900

    sns認証準備完了

--- app/models/motivate_credential.rb
+++ app/models/motivate_credential.rb
@@ -1,3 +1,3 @@
 class MotivateCredential < ApplicationRecord
-     
+     belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
+  has_many :motivate_credentials
   has_many :posts
   has_many :comments
   has_many :goods, dependent: :destroy
